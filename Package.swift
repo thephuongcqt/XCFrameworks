82155f6f97f1acb91d3d740702577250cb34e174
Phuong Nguyen The <phuong.nt@amanotes.com>
2021-08-26 21:54:46 +0700

    xcframeworks

--- Package.swift
+++ Package.swift
@@ -5,24 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "XCFrameworks",
+    platforms: [.iOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "XCFrameworks",
-            targets: ["XCFrameworks"]),
+       .library(name: "XCFrameworks", targets: ["XCFrameworks"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Moya/Moya.git", .branch("development")),
+        .package(url: "https://github.com/RxSwiftCommunity/RxDataSources.git", .upToNextMajor(from: "5.0.0")),        
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "XCFrameworks",
-            dependencies: []),
-        .testTarget(
-            name: "XCFrameworksTests",
-            dependencies: ["XCFrameworks"]),
+            dependencies: [
+                "RxDataSources",  
+                .product(name: "RxMoya", package: "Moya"),
+                .product(name: "RxBlocking", package: "RxSwift"),
+                .product(name: "RxTest", package: "RxSwift")
+            ]
+        ),
     ]
 )
